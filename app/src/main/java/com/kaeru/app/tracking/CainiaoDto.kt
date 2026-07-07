5f1f279beacbcd6912f1af745175478c27b724c4
kkyago <yagorangrl+1@gmail.com>
2026-07-07 13:20:54 -0300

    Fixed Cainiao crash, fixes #22

--- app/src/main/java/com/kaeru/app/tracking/CainiaoDto.kt
+++ app/src/main/java/com/kaeru/app/tracking/CainiaoDto.kt
@@ -5,17 +5,19 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CainiaoResponse(
-    @SerialName("module") val modules: List<CainiaoModule>?,
-    @SerialName("success") val success: Boolean?
+    @SerialName("module") val modules: List<CainiaoModule>? = null,
+    @SerialName("success") val success: Boolean? = null
 )
+
 @Serializable
 data class CainiaoModule(
-    @SerialName("mailNo") val code: String?,
-    @SerialName("detailList") val details: List<CainiaoDetail>?
+    @SerialName("mailNo") val code: String? = null,
+    @SerialName("detailList") val details: List<CainiaoDetail>? = null
 )
+
 @Serializable
 data class CainiaoDetail(
-    @SerialName("timeStr") val dateString: String?,
-    @SerialName("standerdDesc") val statusDescription: String?,
-    @SerialName("desc") val altDescription: String?
+    @SerialName("timeStr") val dateString: String? = null,
+    @SerialName("standerdDesc") val statusDescription: String? = null,
+    @SerialName("desc") val altDescription: String? = null
 )
